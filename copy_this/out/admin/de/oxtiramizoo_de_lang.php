c2b5b07f44c87bb4a8e2a653fbb8ef9ffe1beeec
Gregor Melhorn <gregor.melhorn@googlemail.com>
2012-11-23 13:28:51 +0100

    Update copy_this/out/admin/de/oxtiramizoo_de_lang.php

--- copy_this/out/admin/de/oxtiramizoo_de_lang.php
+++ copy_this/out/admin/de/oxtiramizoo_de_lang.php
@@ -14,61 +14,61 @@ $aLang = array(
 
 // Admin Menu
 'oxTiramizoo_admin_menu_label'                          => 'Tiramizoo',
-'oxTiramizoo_settings'                                  => 'Settings',
+'oxTiramizoo_settings'                                  => 'Einstellungen',
 
 'oxTiramizoo_order_submenu_label'                       => 'Tiramizoo',
 'oxTiramizoo_article_submenu_label'                     => 'Tiramizoo',
 'oxTiramizoo_category_submenu_label'                    => 'Tiramizoo',
 
 //Admin settings
-'oxTiramizoo_settings_save_label'                       => 'Save settings',
+'oxTiramizoo_settings_save_label'                       => 'Einstellungen speichern',
 
-'oxTiramizoo_settings_title'                            => 'Tiramizoo settings',
+'oxTiramizoo_settings_title'                            => 'Tiramizoo Einstellungen',
 
 'oxTiramizoo_settings_api_url_label'                    => 'Tiramizoo URL',
-'oxTiramizoo_settings_api_url_help'                     => 'Tiramizoo URL probably http://api.tiramizoo.com/v1',
+'oxTiramizoo_settings_api_url_help'                     => 'Produktivsystem meistens https://api.tiramizoo.com/v1, Testsystem https://api-sandbox.tiramizoo.com/v1',
 
 'oxTiramizoo_settings_api_key_label'                    => 'Tiramizoo API key',
-'oxTiramizoo_settings_api_key_help'                     => 'Some help to get the Tiramizoo API key',
+'oxTiramizoo_settings_api_key_help'                     => 'Zu finden in Ihrem Nutzerprofil',
 
 'oxTiramizoo_settings_shop_url_label'                   => 'Tiramizoo Shop URL',
-'oxTiramizoo_settings_shop_url_help'                    => 'some help Tiramizoo Shop URL',
+'oxTiramizoo_settings_shop_url_help'                    => 'URL Ihres Webshops',
 
-'oxTiramizoo_settings_shop_address_label'               => 'Shop Address',
-'oxTiramizoo_settings_shop_address_help'                => 'some help address',
+'oxTiramizoo_settings_shop_address_label'               => 'Abholort Straße und Hausnummer',
+'oxTiramizoo_settings_shop_address_help'                => 'z.B. Alexanderplatz 1',
 
-'oxTiramizoo_settings_shop_postal_code_label'           => 'Shop Postal Code',
-'oxTiramizoo_settings_shop_postal_code_help'            => 'some help postal code',
+'oxTiramizoo_settings_shop_postal_code_label'           => 'Abholort Postleitzahl',
+'oxTiramizoo_settings_shop_postal_code_help'            => 'z.B. 10112',
 
-'oxTiramizoo_settings_shop_country_label'               => 'Shop Country',
-'oxTiramizoo_settings_shop_country_help'                => 'some help zip code',
+'oxTiramizoo_settings_shop_country_label'               => 'Abholhort Ländercode',
+'oxTiramizoo_settings_shop_country_help'                => 'z.B. DE',
 
-'oxTiramizoo_settings_shop_contact_name_label'          => 'Shop Contact Name',
-'oxTiramizoo_settings_shop_contact_name_help'           => 'some help shop contact name',
+'oxTiramizoo_settings_shop_contact_name_label'          => 'Abholort Bezeichnung',
+'oxTiramizoo_settings_shop_contact_name_help'           => 'z.B. Testshop GmbH',
 
-'oxTiramizoo_settings_shop_phone_number_label'          => 'Shop Phone Number',
-'oxTiramizoo_settings_shop_phone_number_help'           => 'some help shop phone number',
+'oxTiramizoo_settings_shop_phone_number_label'          => 'Abholort Telefonnummer',
+'oxTiramizoo_settings_shop_phone_number_help'           => 'z.B. +49 30 123456',
 
-'oxTiramizoo_settings_shop_email_address_label'         => 'Shop Email Address',
-'oxTiramizoo_settings_shop_email_address_help'          => 'some help shop email address',
+'oxTiramizoo_settings_shop_email_address_label'         => 'Email',
+'oxTiramizoo_settings_shop_email_address_help'          => 'Email-Adresse für Benachrichtigungen',
 
-'oxTiramizoo_settings_shop_city_label'                  => 'Shop City',
-'oxTiramizoo_settings_shop_city_help'                   => 'some help zip code',
+'oxTiramizoo_settings_shop_city_label'                  => 'Abholort Stadt',
+'oxTiramizoo_settings_shop_city_help'                   => 'z.B. Berlin',
 
-'oxTiramizoo_settings_enable_module_label'              => 'Enable Tiramizoo',
-'oxTiramizoo_settings_enable_module_help'               => 'Help for Enable Tiramizoo',
+'oxTiramizoo_settings_enable_module_label'              => 'Tiramizoo aktivieren',
+'oxTiramizoo_settings_enable_module_help'               => 'Bieten Sie tiramizoo als Lieferoption an',
 
-'oxTiramizoo_settings_pickup_hour_1_label'              => '1st pick up hour',
-'oxTiramizoo_settings_pickup_hour_2_label'              => '2nd pick up hour',
-'oxTiramizoo_settings_pickup_hour_3_label'              => '3rd pick up hour',
-'oxTiramizoo_settings_pickup_hours_help'                => 'You can select 1-3 pickup hours',
+'oxTiramizoo_settings_pickup_hour_1_label'              => '1. Abholzeitpunkt',
+'oxTiramizoo_settings_pickup_hour_2_label'              => '2. Abholzeitpunkt',
+'oxTiramizoo_settings_pickup_hour_3_label'              => '3. Abholzeitpunkt',
+'oxTiramizoo_settings_pickup_hours_help'                => 'Sie können bis zu 3 Abholzeitpunkten wählen',
 
-'oxTiramizoo_pickup_hour_not_specified'                 => 'Not specified',
+'oxTiramizoo_pickup_hour_not_specified'                 => 'Nicht angegeben',
 
-'oxTiramizoo_settings_payment_methods_assigned_label'   => 'Tiramizoo payment methods assigned',
+'oxTiramizoo_settings_payment_methods_assigned_label'   => 'Zahlungsmethoden, mit denen Tiramizoo verwendet werden kann',
 
-'oxTiramizoo_settings_order_to_pickup_offset_label'     => 'Order To Pickup Time offset',
-'oxTiramizoo_settings_order_to_pickup_offset_help'      => 'How many time You need to create pickup',
+'oxTiramizoo_settings_order_to_pickup_offset_label'     => 'Packzeit',
+'oxTiramizoo_settings_order_to_pickup_offset_help'      => 'Wieviel Zeit benötigen Sie zwischen Bestellung und Abholung zum Bereitstellen der Ware?',
 
 'oxtiramizoo_category_tab_label'                        => 'Tiramizoo',
 'oxtiramizoo_article_tab_label'                         => 'Tiramizoo',
