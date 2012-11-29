2b4b7c5c6c99996f117f237c406af3ae992fec1e
Krzysztof Kowalik <kowalikus@gmail.com>
2012-11-29 23:23:04 +0100

    Move most unnecessary logic to special helper class. Use parent call of overriding methods instead ugly hardcode injection.

--- copy_this/modules/oxtiramizoo/core/oxtiramizoo_helper.php
+++ copy_this/modules/oxtiramizoo/core/oxtiramizoo_helper.php
@@ -1,32 +1,38 @@
 <?php
-/**
- * This file is part of the module oxTiramizoo for OXID eShop.
- *
- * The module oxTiramizoo for OXID eShop is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by the Free Software Foundation
- * either version 3 of the License, or (at your option) any later version.
- *
- * The module oxTiramizoo for OXID eShop is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. 
- *  
- * See the GNU General Public License for more details <http://www.gnu.org/licenses/>
- *
- * @copyright: Tiramizoo GmbH
- * @author: Krzysztof Kowalik <kowalikus@gmail.com>
- * @package: oxTiramizoo
- * @license: http://www.gnu.org/licenses/
- * @version: 1.0.0
- * @link: http://tiramizoo.com
- */
 
 /**
  * This class contains static methods used for calculating pickup and delivery hours
  *
  * @package: oxTiramizoo
  */
-class oxTiramizooHelper
+class oxTiramizooHelper extends oxSuperCfg
 {
+    /**
+     * Singleton instance
+     * 
+     * @var oxTiramizooApi
+     */
+    protected static $_instance = null;
+
+    /**
+     * Get the instance of class
+     * 
+     * @return oxTiramizooHelper
+     */
+    public static function getInstance()
+    {
+        if ( !self::$_instance instanceof oxTiramizooHelper ) {
+                self::$_instance = new oxTiramizooHelper();
+        }
+
+        return self::$_instance;
+    }
+
+    /**
+     * @var integer
+     */
+    protected $_isTiramizooAvailable = -1;
+
     /**
      * Convert date time to more readable string
      * 
@@ -50,5 +56,158 @@ class oxTiramizooHelper
         } else {
             return $dateTime . ' - ' . date('H:i', strtotime('+' . $deliveryOffsetTime . 'minutes', strtotime($dateTime)));
         }
+    }
+
+    /**
+     * Get tiramizoo deliver time window selected by user 
+     * 
+     * @return string
+     */
+    public function getSelectedTimeWindow()
+    {
+        return oxSession::getVar( 'sTiramizooTimeWindow' );
+    }
+
+    /**
+     * Get available delivery windows to present in cart
+     * 
+     * @return array
+     */
+    public function getAvailableDeliveryHours()
+    {
+        $oxConfig = $this->getConfig();
+
+        $aAvailableDeliveryHours = array();
+        
+        $orderOffsetTime = (int)$oxConfig->getShopConfVar('oxTiramizoo_order_pickup_offset');
+        $deliveryOffsetTime = (int)$oxConfig->getShopConfVar('oxTiramizoo_pickup_del_offset');
+
+        $dateTime = date('Y-m-d H:i');
+
+        $itertator = 0;
+        while ($itertator++ < 6)
+        {
+            $dateTime = $this->getNextAvailableDate($dateTime);
+
+            $aAvailableDeliveryHours[$dateTime] = oxTiramizooHelper::getLabelDeliveryWindow($dateTime);
+
+            if (($itertator == 1) && !oxSession::hasVar( 'sTiramizooTimeWindow' )) {
+                oxSession::setVar( 'sTiramizooTimeWindow',  $dateTime);
+            }
+        }
+
+        return $aAvailableDeliveryHours;
+    }
+
+    /**
+     * Get available pikup hours from config
+     * 
+     * @return array Array of datetimes
+     */
+    public function getAvailablePickupHours()
+    {
+        $aAvailablePickupHours = array();
+
+        $oxConfig = oxConfig::getInstance();
+
+        for ($i = 1; $i <= 6 ; $i++) 
+        {
+            if ($pickupHour = $oxConfig->getShopConfVar('oxTiramizoo_shop_pickup_hour_' . $i)) {
+                $aAvailablePickupHours[] = $pickupHour;
+            }
+        }
+
+        return $aAvailablePickupHours;
+    }
+
+    /**
+     * Get next available pickup date excluding weekends
+     * 
+     * @param  string $dateTime Date time
+     * @return string Next date time
+     */
+    public function getNextAvailableDate($fromDateTime)
+    {
+        $oxConfig = $this->getConfig();
+
+        $orderOffsetTime = (int)$oxConfig->getShopConfVar('oxTiramizoo_order_pickup_offset');
+
+        $fromDateTime = date('Y-m-d H:i', strtotime('+' . $orderOffsetTime . ' minutes', strtotime($fromDateTime)));
+        $fromHour = date('H:i', strtotime($fromDateTime));
+        $fromDate = date('Y-m-d', strtotime($fromDateTime));
+        $fromDayNum = date('w', strtotime($fromDateTime));
+
+        if (in_array($fromDayNum, range(1, 5))) {
+            foreach ($this->getAvailablePickupHours() as $sAvailablePickupHour) 
+            {
+                if (strtotime($fromHour) < strtotime($sAvailablePickupHour)) {
+                    return $fromDate . ' ' . $sAvailablePickupHour;
+                }
+            }
+        } 
+
+        if (in_array($fromDayNum, array(6))) {
+            $nextDateTime = date('Y-m-d', strtotime('+2days', strtotime($fromDateTime))) . ' 00:00';
+            return $this->getNextAvailableDate($nextDateTime);
+        } else {
+            $nextDateTime = date('Y-m-d', strtotime('+1days', strtotime($fromDateTime))) . ' 00:00';
+            return $this->getNextAvailableDate($nextDateTime);
+        }
+    }
+
+    /**
+     * Validate basket data to decide if can be delivered by tiramizoo 
+     * 
+     * @return bool
+     */
+    public function isTiramizooAvailable() 
+    {
+        if ($this->_isTiramizooAvailable === -1) {
+
+            $oBasket = $this->getSession()->getBasket();
+            $oxConfig = $this->getConfig();
+
+            $oOrder = oxNew( 'oxorder' );
+            $address = $oOrder->getDelAddressInfo();
+
+            $oUser = $this->getUser();
+
+            $sZipCode = $address ? $address->oxaddress__oxzip->value : $oUser->oxuser__oxzip->value;
+
+            if (!$this->getConfig()->getConfigParam('oxTiramizoo_enable_module')) {
+                return $this->_isTiramizooAvailable = 0;
+            }
+
+            if (!count($this->getAvailablePickupHours())) {
+                return $this->_isTiramizooAvailable = 0;
+            }
+
+            //check if Tiramizoo can deliver this basket
+            $data = new stdClass();
+            $data->pickup_postal_code = $this->getConfig()->getConfigParam('oxTiramizoo_shop_postal_code');
+            $data->delivery_postal_code = $sZipCode;
+            $data->items = array();
+
+            require_once getShopBasePath() . '/modules/oxtiramizoo/core/TiramizooApi/oxTiramizooApi.php';
+
+            $data->items = oxTiramizooApi::getInstance()->buildItemsData($oBasket);
+
+            $result = oxTiramizooApi::getInstance()->getQuotes($data);
+
+            if (!in_array($result['http_status'], array(200))) {
+                
+                // Uncomment to debug
+                // echo '<div>';
+                // echo json_encode($data);
+                // echo json_encode($result);
+                // echo '</div>';
+
+                return $this->_isTiramizooAvailable = 0;
+            }
+
+            $this->_isTiramizooAvailable = 1;
+        }
+
+        return $this->_isTiramizooAvailable;
     }
 }
