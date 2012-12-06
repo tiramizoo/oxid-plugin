44a730a3fa189b2d4fc45aeec2da9bd44f19e818
Krzysztof Kowalik <kowalikus@gmail.com>
2012-12-06 13:04:12 +0100

    Added 19:00 as maximum hour to select pickup

--- copy_this/admin/oxtiramizoo_settings.php
+++ copy_this/admin/oxtiramizoo_settings.php
@@ -35,7 +35,7 @@ class oxTiramizoo_settings extends Shop_Config
     $this->_aViewData['aAvailablePickupHours'] = array('9:00', '9:30', '10:00', '10:30', '11:00', '11:30', 
                                                        '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', 
                                                        '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', 
-                                                       '18:00', '18:30');
+                                                       '18:00', '18:30', '19:00');
 
 
     if (count(oxSession::getVar('oxTiramizoo_settings_errors'))) {
