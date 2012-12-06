855c3f853021795c4a9b91587cab9ff28fe66fc8
Krzysztof Kowalik <kowalikus@gmail.com>
2012-12-06 14:54:30 +0100

    Fix the non exists oxTiramizooConfig

--- copy_this/admin/oxtiramizoo_settings.php
+++ copy_this/admin/oxtiramizoo_settings.php
@@ -1,6 +1,12 @@
 <?php
 
-require_once  dirname(__FILE__) . '/../modules/oxtiramizoo/core/oxtiramizoo_setup.php';
+if ( !class_exists('oxTiramizooConfig') ) {
+    require_once getShopBasePath() . '/modules/oxtiramizoo/core/oxtiramizoo_config.php';
+}
+
+if ( !class_exists('oxTiramizooSetup') ) {
+    require_once getShopBasePath() . '/modules/oxtiramizoo/core/oxtiramizoo_setup.php';
+}
 
 /**
  * Tiramizoo settings
