a4baf8a52bafa1cd663afb58d2819d61ab90fdd8
Krzysztof Kowalik <kowalikus@gmail.com>
2013-05-27 11:39:36 +0200

    Remove standard and express delivery price from calculate delivery price methodRemove standard and express delivery price from calculate delivery price method

--- copy_this/modules/oxtiramizoo/core/oxtiramizoo_deliveryprice.php
+++ copy_this/modules/oxtiramizoo/core/oxtiramizoo_deliveryprice.php
@@ -8,31 +8,7 @@ class oxTiramizoo_DeliveryPrice
 		// e.g. The delivery price is depended on time window delivery type and tiramizoo's account prices
 		
 		/*
-			$oRetailLocation = $oTiramizooDeliverySet->getRetailLocation();
-
-			$iStandardPrice = intval($oRetailLocation->getConfVar('standard_price')) / 100;
-			$iExpressPrice = intval($oRetailLocation->getConfVar('express_price')) / 100;
-
-			if ($iWeekendSurchargePercent = intval($oRetailLocation->getConfVar('weekend_surcharge_percent'))) {
-				$iStandardWeekendPrice = $iStandardPrice * ((100 + $iWeekendSurchargePercent) / 100);
-				$iExpressWeekendPrice = $iExpressPrice * ((100 + $iWeekendSurchargePercent) / 100);
-			}
-
-			$oTimeWindow = $oTiramizooDeliverySet->getSelectedTimeWindow();
-
-			if (($oTimeWindow->getDeliveryType() == 'standard') && isset($iStandardPrice)) {
-				$iPrice = $iStandardPrice;
-			} else if (($oTimeWindow->getDeliveryType() == 'express') && isset($iExpressPrice)) {
-				$iPrice = $iExpressPrice;
-			} else if (($oTimeWindow->getDeliveryType() == 'standard_weekend') && isset($iStandardWeekendPrice)) {
-				$iPrice = $iStandardWeekendPrice;
-			} else if (($oTimeWindow->getDeliveryType() == 'express_weekend') && isset($iExpressWeekendPrice)) {
-				$iPrice = $iExpressWeekendPrice;
-			}
-
-			if (isset($iPrice)) {
-				$oDeliveryPrice->setPrice($iPrice);
-			}
+			$oDeliveryPrice->setPrice(10);
 
 		*/
 		
